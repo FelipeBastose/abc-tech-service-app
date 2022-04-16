46b7d76993b40b8a0e0b3051e57c4eac2ba56aa8
Felipe Bastos <felipeevangelista@movida.com.br>
2022-04-15 23:59:13 -0300

    first commit app fiap

---
+++ lib/pages/home_bind.dart
@@ -0,0 +1,15 @@
+///home_bind.dart
+
+import 'package:abctechapp/controller/assistance_controller.dart';
+import 'package:get/get.dart';
+
+class HomeBind extends Bindings {
+  @override
+  void dependencies() {
+
+    Get.lazyPut<AssistanceController>(() => AssistanceController());
+  
+  }
+
+  
+}
